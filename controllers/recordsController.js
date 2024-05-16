edd3fe0cf1454f69533a70c510e023798f430279
vovashik0310 <v9873826@gmail.com>
2024-05-16 20:15:41 +0300

    complete the records controller

---
+++ controllers/recordsController.js
@@ -0,0 +1,15 @@
+exports.readRecord = (req, res) => {
+  res.send("You can read the records");
+};
+
+exports.createRecord = (req, res) => {
+  res.send("You can create the records");
+};
+
+exports.deleteRecord = (req, res) => {
+  res.send("You can delete the records");
+};
+
+exports.updateRecord = (req, res) => {
+  res.send("You can update the records");
+};
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const connectDB = require("./config/db");
 const app = express();
 
 const authRouter = require("./routes/auth");
+const recordRouter = require("./routes/record");
 
 app.use(express.json());
 app.use("/auth", authRouter);
+app.use("/api", recordRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
